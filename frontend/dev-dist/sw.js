ade33b7fbd47f03cb712b75e7b19747a54f9d4ba
Trideep <trideepkumar.h@gmail.com>
2026-05-01 15:51:07 +0530

    issue

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.cstplg6id0o"
+    "revision": "0.esle9ihtkd4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
